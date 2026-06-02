382659ced148bc3962fe32f64205d80979255912
Maks4u <kabaneri15@gmail.com>
2026-06-02 21:43:43 +0300

    Add openAPI dependency
    (cherry picked from commit 92f2311d21bf136f851350fe9adc73d4e794ec34)

--- src/main/java/ua/mtb/inverse/contracts/enums/IssueCurrency.java
+++ src/main/java/ua/mtb/inverse/contracts/enums/IssueCurrency.java
@@ -1,7 +1,20 @@
 package ua.mtb.inverse.contracts.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+        description =
+                """
+                Валюта випуску облігації.
+        
+                Можливі значення:
+                - UAH — гривня;
+                - USD — долар США;
+                - EUR — євро.
+                """
+)
 public enum IssueCurrency {
-  USD,
   UAH,
+  USD,
   EUR
 }
