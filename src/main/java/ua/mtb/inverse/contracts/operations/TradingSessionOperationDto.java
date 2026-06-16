394f8c951af7a0ab591503b990ef237b53596539
Maks4u <kabaneri15@gmail.com>
2026-06-16 18:59:25 +0300

    [INVRS-30] upd: Bump contracts version to 1.0.19

---
+++ src/main/java/ua/mtb/inverse/contracts/operations/TradingSessionOperationDto.java
@@ -0,0 +1,41 @@
+package ua.mtb.inverse.contracts.operations;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ua.mtb.inverse.contracts.enums.IssueCurrency;
+import ua.mtb.inverse.contracts.enums.OperationStatus;
+import ua.mtb.inverse.contracts.enums.SessionType;
+import ua.mtb.inverse.contracts.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TradingSessionOperationDto {
+  private Long id;
+  private TransactionType transactionType;
+  private Long operationId;
+  private OffsetDateTime createdAt;
+  private String clientName;
+  private String identifyCode;
+  private String isin;
+  private Integer isinQuantity;
+  private OperationStatus status;
+  private SessionType sessionType;
+  private BigDecimal totalPrice;
+  private String operationAgreementNumber;
+  private String operationAgreementDate;
+  private String generalAgreementNumber;
+  private String generalAgreementDate;
+  private String custodianAccountNumber;
+  private String contragentId;
+  private String diiaId;
+  private IssueCurrency currency;
+  private LocalDate maturityDate;
+}
