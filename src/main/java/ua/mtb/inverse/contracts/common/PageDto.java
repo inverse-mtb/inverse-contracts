c634d6abb3c9f32881b6a915875c78a549ffaf8f
Kostiantyn <kshuba@mtb.ua>
2026-07-16 09:27:05 +0200

    [INVRS-00] upd: Bump contracts version to 1.0.45

---
+++ src/main/java/ua/mtb/inverse/contracts/common/PageDto.java
@@ -0,0 +1,15 @@
+package ua.mtb.inverse.contracts.common;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageDto<T> {
+    private List<T> content;
+    private PageMetadata page;
+}
