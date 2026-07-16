4657599722613593d14c67c867d435f625065a3e
Kostiantyn <kshuba@mtb.ua>
2026-07-16 09:27:05 +0200

    [INVRS-00] upd: Bump contracts version to 1.0.45

---
+++ src/main/java/ua/mtb/inverse/contracts/common/PageMetadata.java
@@ -0,0 +1,15 @@
+package ua.mtb.inverse.contracts.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageMetadata {
+    private int size;
+    private int number;
+    private long totalElements;
+    private int totalPages;
+}
