e36df2e362737234abe9c8ae61e0bfa2e145bcf3
Kostiantyn <kshuba@mtb.ua>
2026-07-28 08:58:25 +0200

    [INVRS-00] upd: Bump contracts version to 1.0.56

--- src/main/java/ua/mtb/inverse/contracts/enums/ExportFormat.java
+++ src/main/java/ua/mtb/inverse/contracts/enums/ExportFormat.java
@@ -1,6 +1,14 @@
 package ua.mtb.inverse.contracts.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ExportFormat {
-  PDF,
-  XLSX
+  PDF(".pdf"),
+  XML(".xml"),
+  XLSX(".xlsx");
+
+  private final String extension;
 }
