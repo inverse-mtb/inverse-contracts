b28a1122f619714d9d5fe2ac9637e187bcd43d51
Kostiantyn <kshuba@mtb.ua>
2026-08-06 23:11:07 +0200

    [INVRS-120] upd: Bump contracts version to 1.0.70

--- src/main/java/ua/mtb/inverse/contracts/kafka/trading/TradingSessionEmailMessage.java
+++ src/main/java/ua/mtb/inverse/contracts/kafka/trading/TradingSessionEmailMessage.java
@@ -14,6 +14,14 @@ public class TradingSessionEmailMessage {
   /** ID запису з trading_session_operations */
   private Long operationId;
 
+  /** Дата вкладання договору з trading_session_operations */
+  private String operationAgreementDate;
+
+  /** Номер договору з trading_session_operations */
+  private String operationAgreementNumber;
+
+  private String clientName;
+
   private String diiaId;
 
   /** Повна інформація по торговій сесії. */
