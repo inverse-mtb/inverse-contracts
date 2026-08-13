ce30f3ca6d2f564d82fe7a2c52d51fd2f55953ef
Kostiantyn <kshuba@mtb.ua>
2026-08-13 21:27:53 +0200

    [INVRS-124] upd: Bump contracts version to 1.0.82

--- src/main/java/ua/mtb/inverse/contracts/SummaryErrorReportData.java
+++ src/main/java/ua/mtb/inverse/contracts/SummaryErrorReportData.java
@@ -1,7 +1,6 @@
 package ua.mtb.inverse.contracts;
 
 import java.math.BigDecimal;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SummaryErrorReportData {
-    private Integer agreementNumber;
-    private String purchaseAgreementDate;
-    private String operationType;
-    private String identifyCode;
-    private String isin;
-    private BigDecimal totalPrice;
-    private Integer quantity;
+  private Integer agreementNumber;
+  private String purchaseAgreementDate;
+  private String operationType;
+  private String identifyCode;
+  private String isin;
+  private BigDecimal totalPrice;
+  private Integer quantity;
 }
