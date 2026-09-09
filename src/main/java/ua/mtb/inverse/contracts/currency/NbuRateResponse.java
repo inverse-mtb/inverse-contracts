b8681e6ef79182e011ccf978541275100d361835
Kostiantyn <kshuba@mtb.ua>
2026-09-09 20:21:39 +0200

    [INVRS-138] upd: Bump contracts version to 1.0.94

--- src/main/java/ua/mtb/inverse/contracts/currency/NbuRateResponse.java
+++ src/main/java/ua/mtb/inverse/contracts/currency/NbuRateResponse.java
@@ -1,12 +1,10 @@
 package ua.mtb.inverse.contracts.currency;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
